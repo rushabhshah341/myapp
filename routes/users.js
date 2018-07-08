8f7e37421e605f5c9e81b5a92adb234dfa6b3ba8
rushabhshah341 <rushabhshah341@gmail.com>
2018-07-08 14:57:19 -0500

    kickstart node express app with demo route

---
+++ routes/users.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/mydata', function(req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+  var data = {
+    name: 'Ellisa',
+    address:'2501 Alton park ave',
+    phoneNumber: '518-423-7196'
+  };
+  res.json(data);
+});
+module.exports = router;
